aec6c02a0f95851590c05d33ba5efb0c18676a21
Barry Gu <barry.gu@ftid.cn>
2014-08-13 21:05:37 +0800

    add content parser

--- parse.py
+++ parse.py
@@ -1,7 +1,7 @@
 #!/bin/python
 # -*-coding:gbk-*-
 
-import os, re
+import os, re, sys
 import httplib
 from sgmllib import SGMLParser
 
@@ -29,7 +29,7 @@ class ListParser(SGMLParser):
 
 	def handle_data(self, data):
 		txt = data.strip()
-		if self.get_start == True and self.href and txt:
+		if self.href and txt:
 			print '%s %s' % (self.href, txt)
 			self.href = None
 
@@ -55,3 +55,51 @@ class ListParser(SGMLParser):
 					break
 
 ListParser().feed(data)
+
+class ContentParser(SGMLParser):
+	get_start = False
+	pre_data = False
+	
+	def start_div(self,attrs):
+		if not self.get_start:
+			for k, v in attrs:
+				if k == 'id' and re.match('content', v.strip(), re.I):
+					self.get_start = True
+					break
+
+	def start_p(self,attrs):
+		if self.get_start:
+			self.pre_data = True
+
+	def end_p(self):
+		if self.pre_data:
+			self.pre_data = False
+
+	def handle_entityref(self, name):
+		if name == 'nbsp':
+			sys.stdout.write(' ')
+		
+	def handle_data(self, data):
+		txt = data.strip()
+		if self.pre_data and txt:
+			print '%s' % (txt)
+
+fname = "524186.html"
+
+if os.path.exists(fname):
+	afile = open(fname, 'r')
+	data = afile.read()
+	afile.close()
+else:
+	conn = httplib.HTTPConnection("www.lwxs.org")
+	conn.request("GET", "/books/2/2754/524186.html")
+	resp = conn.getresponse()
+	if resp.status == 200:
+		data = resp.read()
+		afile = open(fname, 'w')
+		afile.write(data)
+		afile.close()
+	resp.close()
+	conn.close()
+
+ContentParser().feed(data)
