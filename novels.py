dbfedadd02788f400aee75a12116e1c89eac2e83
Barry Gu <barry.gu@ftid.cn>
2014-08-14 20:03:15 +0800

    use func to instead duplicate code

--- novels.py
+++ novels.py
@@ -2,44 +2,17 @@
 # -*-coding:gbk-*-
 
 import os, re
-import httplib
+import myfuncs
 
 fname = "chapters.html"
-
-def chinese2digits(uchars_chinese):
-	common_used_numerals = {u'零':0, u'一':1, u'二':2, u'三':3, u'四':4, u'五':5, u'六':6, u'七':7, u'八':8, u'九':9, u'十':10, u'百':100, u'千':1000, u'万':10000, u'亿':100000000}
-	total = 0
-	r = 1
-
-	for i in range(len(uchars_chinese) - 1, -1, -1):
-		x = common_used_numerals.get(uchars_chinese[i], 0)
-		if x >= 10:
-			if x > r:
-				r = x
-			else:
-				r = r * x
-			if i == 0:
-				total += r
-		else:
-			total += r * x
-
-	return total
 
 if os.path.exists(fname):
 	afile = open(fname, 'r')
 	data = afile.read()
 	afile.close()
 else:
-	conn = httplib.HTTPConnection("www.lwxs.org")
-	conn.request("GET", "/books/2/2754/index.html")
-	resp = conn.getresponse()
-	if resp.status == 200:
-		data = resp.read()
-		afile = open(fname, 'w')
-		afile.write(data)
-		afile.close()
-	resp.close()
-	conn.close()
+	data = myfuncs.get_page('http://www.lwxs.org/books/2/2754/index.html')
+	myfuncs.save_file(fname, data)
 
 regexp = "<TD class=bookinfo_td .*>"
 pattern = re.compile(regexp, re.I)
@@ -73,7 +46,7 @@ for item in pattern.finditer(result[0]):
 
 	m = p3.search(t2)
 	if m:
-		t = chinese2digits(m.group(1))
+		t = myfuncs.chinese2digits(m.group(1))
 		print m.group(), t
 
 bfile.close()
